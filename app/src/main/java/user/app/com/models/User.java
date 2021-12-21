afdc0339d5f31b61aae55fb5ea9b9f012dd188e3
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-12-21 16:48:23 +0200

    first commit

---
+++ app/src/main/java/user/app/com/models/User.java
@@ -0,0 +1,54 @@
+package user.app.com.models;
+
+public class User {
+    private String id, name, imgUrl, biography;
+
+    public User(String id, String name, String imgUrl, String biography) {
+        this.id = id;
+        this.name = name;
+        this.imgUrl = imgUrl;
+        this.biography = biography;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", imgUrl='" + imgUrl + '\'' +
+                ", biography='" + biography + '\'' +
+                '}';
+    }
+}
